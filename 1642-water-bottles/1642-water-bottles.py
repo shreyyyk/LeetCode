9f5e16c6f6ed85ed368f0df630228ffee4e34be9
Shreyansh Sankrit <101209894+shreyyyk@users.noreply.github.com>
2024-07-07 14:41:59 +0530

    Time: 33 ms (64.44%), Space: 16.5 MB (79.17%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.py
+++ 1642-water-bottles/1642-water-bottles.py
@@ -1,9 +1,11 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        ans=numBottles
-        while(numBottles>numExchange-1):
-            ex=numBottles//numExchange
-            rem=numBottles%numExchange
-            ans+=ex
-            numBottles=ex+rem
-        return ans
+        drank,empty=0,0
+        while(numBottles):
+            drank+=1
+            empty+=1
+            numBottles-=1
+            if empty==numExchange:
+                empty-=numExchange
+                numBottles+=1
+        return drank
