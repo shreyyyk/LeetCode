fca999f238ccb29e9368cadfbcb7b49e14a621cb
Shreyansh Sankrit <101209894+shreyyyk@users.noreply.github.com>
2024-08-06 12:02:54 +0530

    Time: 325 ms (5.01%), Space: 17.6 MB (36.75%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        hashy={}
+        for i in range(len(word)):
+            if word[i] in hashy:
+                hashy[word[i]]+=1
+            else:
+                hashy[word[i]]=1
+        print(hashy)
+        if len(hashy.keys())<=8:
+            return sum(hashy.values())
+        else:
+            lst=sorted(hashy.values(), reverse=True)
+            count=0
+            mul=1
+            maxx=8
+            while lst:
+                curr=lst[:8]
+                count+=(sum(curr)*mul)
+                lst=lst[8:]
+                mul+=1
+        return count
+            
